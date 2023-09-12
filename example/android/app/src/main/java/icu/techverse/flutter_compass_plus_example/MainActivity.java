b364113c0df410d79ecf1f70243b755ce5861965
Grzegorz Fraczek <s17476@pjwstk.edu.pl>
2023-09-12 21:34:48 +0200

    Android added

---
+++ example/android/app/src/main/java/icu/techverse/flutter_compass_plus_example/MainActivity.java
@@ -0,0 +1,5 @@
+package icu.techverse.flutter_compass_plus_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {}
--- example/android/app/src/main/kotlin/icu/techverse/flutter_compass_plus_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package icu.techverse.flutter_compass_plus_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
